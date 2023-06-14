e1cda0b450261575d4d81f3dd3edc86dd92e665e
Elie Toubiana <elie.toubiana@gmail.com>
2023-06-14 13:22:33 -0700

    better

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   before_action :authenticate_user!, only: [:dashboard]
+  layout "landing", only: [:index]
 
   def index
   end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def flash_class(type)
+    case type.to_sym
+    when :notice
+      "bg-green-500 text-white px-4 py-4 rounded m-2"
+    when :alert
+      "bg-red-500 text-white px-4 py-4 rounded m-2"
+    else
+      "bg-blue-500 text-white px-4 py-4 rounded m-2"
+    end
+  end
 end
